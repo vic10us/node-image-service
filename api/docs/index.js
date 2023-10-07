3174ae2ed8d1881283f96ec4496fbdd69e561ad7
vic10us <code@dexs.ca>
2023-10-07 05:16:43 -0400

    bumping version in api-docs

--- api/docs/index.js
+++ api/docs/index.js
@@ -1,17 +1,9 @@
 const basicInfo = require('./basicInfo');
-const servers = require('./servers');
 const components = require('./components');
 const tags = require('./tags');
-const loremBarnak = require('./loremBarnak');
-const images = require('./images');
 
 module.exports = {
     ...basicInfo,
     ...components,
     ...tags,
 };
-
-// ...servers,
-// ...loremBarnak,
-// ...images,
-//    ...todos
